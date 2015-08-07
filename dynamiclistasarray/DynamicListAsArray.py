efae4a84a53a4e7941619696b366c3a733f17612
Sean Thomas Canavan <seantcanavan@gmail.com>
2015-08-07 07:33:08 -0700

    Fix to growth and shrink algorithms
    Growth and shrink happened at the same ratio. this makes multiple adds /
    removals at the barrier inefficient and will create lots of needless
    shrinking and growing. growth factor is still 2 but shrink factor is now
    3.
    
    Fixed a bug where the list would grow on removal. Fixed by refactoring
    double_size into shrink and grow which are called independently on
    removal and insertion respectively.

--- dynamiclistasarray/DynamicListAsArray.py
+++ dynamiclistasarray/DynamicListAsArray.py
@@ -4,11 +4,68 @@ class DynamicListAsArray:
     """A dynamic list ADT with array as the implementation"""
     STARTING_SIZE = 10
     GROWTH_FACTOR = 2
+    SHRINK_FACTOR = 3
     content = []
     position = -1
 
     def __init__(self):
         self.content = [-1 for x in range(self.STARTING_SIZE)]
+        print("initialized")
+        self.print_contents()
+
+    def get(self):
+        """O(1)"""
+        return self.content[self.position + 1]
+
+    def get_at(self, pos):
+        """O(1)"""
+        if pos > self.position + 1:
+            pos = self.position + 1
+        return self.content[pos]
+
+    def insert(self, val):
+        """O(N) worst case on resize"""
+        self.grow_array()
+        self.position += 1
+        self.content[self.position] = val
+        self.print_contents()
+
+    def insert_at(self, val, pos):
+        """O(N) worst case on resize"""
+        self.grow_array()
+        if pos > self.position + 1:
+            pos = self.position + 1
+        self.content[pos] = val
+        self.position += 1
+        self.print_contents()
+
+    def remove(self):
+        """O(N) worst case on resize"""
+        if self.position != -1:
+            self.position -= 1
+            self.shrink_array()
+            self.print_contents()
+        else:
+            print("can't remove. nothing to remove")
+
+    def remove_at(self, pos):
+        """O(N) worst case on resize"""
+        if self.position == -1:
+            print("can't remove. nothing to remove")
+        else:
+            self.shrink_array()
+            if pos > self.position + 1:
+                pos = self.position + 1
+            for x in range(pos, len(self.content) -1):
+                self.content[x] = self.content[x+1]
+            self.position -= 1
+            self.print_contents()
+
+    def reset(self):
+        """O(N) to rebuilt the array"""
+        self.content = [-1 for x in range(0, 10)]
+        self.position = -1
+        print("reset")
         self.print_contents()
 
     def print_contents(self):
@@ -22,63 +79,25 @@ class DynamicListAsArray:
             content_string += "|"
         print(content_string + len_string)
 
+    def grow_array(self):
+      if self.position == len(self.content) -1:
+            new_array = [-1 for x in range(len(self.content) * self.GROWTH_FACTOR)]
+            for x in range(0, len(self.content)):
+                new_array[x] = self.content[x]
+            self.content = new_array
+
+    def shrink_array(self):
+        if (len(self.content) / self.SHRINK_FACTOR) > self.position and (len(self.content) > self.STARTING_SIZE):
+            new_array = [-1 for x in range((int(len(self.content) / self.GROWTH_FACTOR)))]
+            for x in range(0, len(new_array)):
+                new_array[x] = self.content[x]
+            self.content = new_array
+
     def get_size(self):
-        print("{" + str(len(self.content)) + "}")
+        return self.position + 1
+
+    def get_max_size(self):
+        return len(self.content)
 
     def get_position(self):
         return self.position
-
-    def get(self, pos):
-        return self.content[pos]
-
-    def double_size(self):
-        print("double_size")
-        new_array = [-1 for x in range(len(self.content) * self.GROWTH_FACTOR)]
-        for x in range(0, len(self.content)):
-            new_array[x] = self.content[x]
-        self.content = new_array
-
-    def shrink_size(self):
-        print("shrink_size")
-        new_array = [len(self.content) / self.GROWTH_FACTOR]
-        for x in range(0, len(new_array)):
-            new_array[x] = self.content[x]
-        self.content = new_array
-
-    def guarantee_size(self):
-        if self.position == len(self.content) -1:
-            self.double_size()
-            print("size multiplied by " + str(self.GROWTH_FACTOR))
-        elif (len(self.content) / self.GROWTH_FACTOR) > self.position and (len(self.content) > 10):
-            self.shrink_size()
-            print("size divided by " + str(self.GROWTH_FACTOR))
-
-    def insert(self, val):
-        self.guarantee_size()
-        self.position += 1
-        self.content[self.position] = val
-        self.print_contents()
-
-    def insert_at(self, val, pos):
-        self.guarantee_size()
-        if pos > self.position + 1:
-            pos = self.position + 1
-        self.content[pos] = val
-        self.position += 1
-        self.print_contents()
-
-    def remove(self):
-        if self.position != -1:
-            self.position -= 1
-            self.print_contents()
-        else:
-            print("can't remove. nothing to remove")
-
-    def remove_at(self, pos):
-        if self.position == -1:
-            print("can't remove. nothing to remove")
-        else:
-            for x in range(pos, len(self.content) -1):
-                self.content[x] = self.content[x+1]
-            self.position -= 1
-            self.print_contents()
--- dynamiclistasarray/DynamicListAsArrayTest.py
+++ dynamiclistasarray/DynamicListAsArrayTest.py
@@ -2,59 +2,65 @@ __author__ = 'advoc'
 
 import unittest
 import DynamicListAsArray
+import random
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         dynamic_list = DynamicListAsArray.DynamicListAsArray()
-        dynamic_list.insert(4)
-        dynamic_list.insert(10)
-        dynamic_list.insert(2)
-        dynamic_list.insert(3)
-        dynamic_list.insert(1)
-        dynamic_list.insert(6)
-        dynamic_list.insert(5)
-        dynamic_list.insert(8)
-        dynamic_list.insert(5)
-        dynamic_list.insert(9)
-        self.assertEqual(dynamic_list.get(9), 9) # assert 10th item = 9
-        dynamic_list.remove()
-        dynamic_list.remove()
-        dynamic_list.remove()
-        dynamic_list.remove()
-        dynamic_list.remove()
-        dynamic_list.remove()
-        dynamic_list.remove()
-        dynamic_list.remove()
-        dynamic_list.remove()
-        dynamic_list.remove()
+        print("insert at the end")
+        dynamic_list.insert(4) #1
+        dynamic_list.insert(6) #2
+        dynamic_list.insert(2) #3
+        dynamic_list.insert(3) #4
+        dynamic_list.insert(1) #5
+        dynamic_list.insert(6) #6
+        dynamic_list.insert(5) #7
+        dynamic_list.insert(8) #8
+        dynamic_list.insert(5) #9
+        dynamic_list.insert(9) #10
+        self.assertEqual(dynamic_list.get_at(9), 9) # assert 10th item = 9
+        print("remove from the end")
+        dynamic_list.remove() #1
+        dynamic_list.remove() #2
+        dynamic_list.remove() #3
+        dynamic_list.remove() #4
+        dynamic_list.remove() #5
+        dynamic_list.remove() #6
+        dynamic_list.remove() #7
+        dynamic_list.remove() #8
+        dynamic_list.remove() #9
+        dynamic_list.remove() #10
         self.assertEqual(dynamic_list.get_position(), -1) # the position is reset to starting
         dynamic_list.remove()
         self.assertEqual(dynamic_list.get_position(), -1) # the position hasn't moved since there's nothing to remove
-        dynamic_list.insert_at(4, 0)
-        dynamic_list.insert_at(10, 1)
-        dynamic_list.insert_at(2, 2)
-        dynamic_list.insert_at(3, 3)
-        dynamic_list.insert_at(1, 4)
-        dynamic_list.insert_at(6, 5)
-        dynamic_list.insert_at(5, 6)
-        dynamic_list.insert_at(8, 7)
-        dynamic_list.insert_at(5, 8)
-        dynamic_list.insert_at(9, 9)
-        self.assertEqual(dynamic_list.get(9), 9) # assert 10th item = 9
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        dynamic_list.remove_at(0)
-        self.assertEqual(dynamic_list.get(0), 9) # the last value is now the first
+        print("insert along the end")
+        dynamic_list.insert_at(4, 0)  #1
+        dynamic_list.insert_at(10, 1) #2
+        dynamic_list.insert_at(2, 2)  #3
+        dynamic_list.insert_at(3, 3)  #4
+        dynamic_list.insert_at(1, 4)  #5
+        dynamic_list.insert_at(6, 5)  #6
+        dynamic_list.insert_at(5, 6)  #7
+        dynamic_list.insert_at(8, 7)  #8
+        dynamic_list.insert_at(5, 8)  #9
+        dynamic_list.insert_at(9, 9)  #10
+        self.assertEqual(dynamic_list.get_at(9), 9) # assert 10th item = 9
+        print("removing from the start")
+        dynamic_list.remove_at(0) #1
+        dynamic_list.remove_at(0) #2
+        dynamic_list.remove_at(0) #3
+        dynamic_list.remove_at(0) #4
+        dynamic_list.remove_at(0) #5
+        dynamic_list.remove_at(0) #6
+        dynamic_list.remove_at(0) #7
+        dynamic_list.remove_at(0) #8
+        dynamic_list.remove_at(0) #9
+        self.assertEqual(dynamic_list.get_at(0), 9) # the last value is now the first
+        dynamic_list.remove_at(0) #
         self.assertEqual(dynamic_list.get_position(), -1) # the position is reset
         dynamic_list.remove_at(0)
         self.assertEqual(dynamic_list.get_position(), -1) # the position hasn't moved since there's nothing to remove
+        print("insert at the end")
         dynamic_list.insert(4)
         dynamic_list.insert(10)
         dynamic_list.insert(2)
@@ -65,11 +71,39 @@ class MyTestCase(unittest.TestCase):
         dynamic_list.insert(8)
         dynamic_list.insert(5)
         dynamic_list.insert(9)
-        print(dynamic_list.get_position())
+        print("expand the list")
         dynamic_list.insert(11)
         self.assertEqual(dynamic_list.get_position(), 10)
+        print("insert at the end")
+        dynamic_list.insert(12)
+        dynamic_list.insert(13)
+        dynamic_list.insert(14)
+        dynamic_list.insert(15)
+        dynamic_list.insert(16)
+        dynamic_list.insert(12)
+        dynamic_list.insert(13)
+        dynamic_list.insert(14)
+        dynamic_list.insert(15)
+        self.assertEqual(dynamic_list.get_size(), 20)
+        self.assertEqual(dynamic_list.get_max_size(), 20)
+        print("expand the list")
+        dynamic_list.insert(21)
+        self.assertEqual(dynamic_list.get_max_size(), 40)
+        dynamic_list.insert_at(22, 30) # there is nothing defined at position 30 so insertion takes place at the end
+        self.assertEqual(dynamic_list.get_at(21), 22)
+        dynamic_list.remove_at(30)
+        dynamic_list.remove_at(30)
+        dynamic_list.remove_at(30)
+        dynamic_list.remove_at(30)
+        dynamic_list.remove_at(30)
+        dynamic_list.remove_at(30)
+        dynamic_list.remove_at(30)
+        dynamic_list.remove_at(30)
+        print("shrink the list")
+        dynamic_list.remove_at(11)
+        dynamic_list.reset()
+        random.seed()
         return True
-
 
 if __name__ == '__main__':
     unittest.main()
