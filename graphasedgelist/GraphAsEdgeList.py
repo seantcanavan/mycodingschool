ceb32d6d6b10258d7ae43a2ec02170b2bc831084
Sean Thomas Canavan <seantcanavan@gmail.com>
2015-11-04 19:28:42 -0800

    finished print_all_edges(), print_all_vertices(), are_vertices_connected(), get_adjacent_vertices()

--- graphasedgelist/GraphAsEdgeList.py
+++ graphasedgelist/GraphAsEdgeList.py
@@ -4,21 +4,38 @@ import dynamiclistasarray.DynamicListAsArray as arraylist
 # space complexity = O ( |e| + |v| )
 
 class GraphAsEdgeList:
-    vertex_list = arraylist()
-    edge_list = arraylist()
+    vertex_list = arraylist.DynamicListAsArray()
+    edge_list = arraylist.DynamicListAsArray()
 
     def add_vertex(self, input_vertex):
-        self.vertex_list.append(input_vertex)
+        self.vertex_list.add(input_vertex)
 
     def add_edge(self, input_edge):
-        self.edge_list.append(input_edge)
+        self.edge_list.add(input_edge)
 
-    #O ( |e| )
-    def find_all_adjactent_nodes(self):
-        print("hi")
+    # O( |e| )
+    def get_adjancent_vertices(self, vertex):
+        adjacent_vertices = []
+        for x in range(0, self.edge_list.get_size()):
+            current_edge = self.edge_list.get_at(x)
+            if current_edge.start.value == vertex.value:
+                adjacent_vertices.append(current_edge.end)
+        return adjacent_vertices
 
+    # O( |v| )
+    def are_vertices_connected(self, vertex1, vertex2):
+        for x in range(0, self.edge_list.get_size()):
+            current_edge = self.edge_list.get_at(x)
+            if vertex1.value == current_edge.start.value and vertex2.value == current_edge.end.value:
+                return True
+        return False
 
-    def are_vertexes_connected(self, vertex1, vertex2):
-        print("hi")
+    def print_all_edges(self):
+        for x in range(0, self.edge_list.get_size()):
+            current = self.edge_list.get_at(x)
+            print("start: " + current.start.value + " end: " + current.end.value)
 
-
+    def print_all_vertices(self):
+        for x in range(0, self.vertex_list.get_size()):
+            current = self.vertex_list.get_at(x)
+            print("vertex: " + current.value)
--- graphasedgelist/GraphAsEdgeListTest.py
+++ graphasedgelist/GraphAsEdgeListTest.py
@@ -6,18 +6,18 @@ import graphasedgelist.GraphAsEdgeList as graph
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        el_graph = graph()
+        el_graph = graph.GraphAsEdgeList()
         # social media people
-        rama = vertex("rama")
-        ella = vertex("ella")
-        bob = vertex("bob")
-        tom = vertex("tom")
-        lee = vertex("lee")
-        sam = vertex("sam")
-        katie = vertex("katie")
-        zahir = vertex("zahir")
-        swati = vertex("swati")
-        arun = vertex("arun")
+        rama = vertex.Vertex("rama")
+        ella = vertex.Vertex("ella")
+        bob = vertex.Vertex("bob")
+        tom = vertex.Vertex("tom")
+        lee = vertex.Vertex("lee")
+        sam = vertex.Vertex("sam")
+        katie = vertex.Vertex("katie")
+        zahir = vertex.Vertex("zahir")
+        swati = vertex.Vertex("swati")
+        arun = vertex.Vertex("arun")
 
         el_graph.add_vertex(rama)
         el_graph.add_vertex(ella)
@@ -31,25 +31,25 @@ class MyTestCase(unittest.TestCase):
         el_graph.add_vertex(arun)
 
         # rama has three friends
-        rta = edge(rama, ella)
-        rtb = edge(rama, bob)
-        rtk = edge(rama, katie)
+        rta = edge.UnweightedEdge(rama, ella)
+        rtb = edge.UnweightedEdge(rama, bob)
+        rtk = edge.UnweightedEdge(rama, katie)
         el_graph.add_edge(rta)
         el_graph.add_edge(rtb)
         el_graph.add_edge(rtk)
 
         # ella has two friends
-        etr = edge(ella, rama)
-        etb = edge(ella, bob)
+        etr = edge.UnweightedEdge(ella, rama)
+        etb = edge.UnweightedEdge(ella, bob)
         el_graph.add_edge(etr)
         el_graph.add_edge(etb)
 
         # bob has 5 friends
-        bte = edge(bob, ella)
-        btr = edge(bob, rama)
-        btl = edge(bob, lee)
-        bts = edge(bob, sam)
-        btt = edge(bob, tom)
+        bte = edge.UnweightedEdge(bob, ella)
+        btr = edge.UnweightedEdge(bob, rama)
+        btl = edge.UnweightedEdge(bob, lee)
+        bts = edge.UnweightedEdge(bob, sam)
+        btt = edge.UnweightedEdge(bob, tom)
         el_graph.add_edge(bte)
         el_graph.add_edge(btr)
         el_graph.add_edge(btl)
@@ -57,17 +57,17 @@ class MyTestCase(unittest.TestCase):
         el_graph.add_edge(btt)
 
         # tom has two friends
-        ttb = edge(tom, bob)
-        tts = edge(tom, sam)
+        ttb = edge.UnweightedEdge(tom, bob)
+        tts = edge.UnweightedEdge(tom, sam)
         el_graph.add_edge(ttb)
         el_graph.add_edge(tts)
 
         # lee has 5 friends
-        ltb = edge(lee, bob)
-        lts = edge(lee, sam)
-        ltz = edge(lee, zahir)
-        lts = edge(lee, swati)
-        ltk = edge(lee, katie)
+        ltb = edge.UnweightedEdge(lee, bob)
+        lts = edge.UnweightedEdge(lee, sam)
+        ltz = edge.UnweightedEdge(lee, zahir)
+        lts = edge.UnweightedEdge(lee, swati)
+        ltk = edge.UnweightedEdge(lee, katie)
         el_graph.add_edge(ltb)
         el_graph.add_edge(lts)
         el_graph.add_edge(ltz)
@@ -75,11 +75,11 @@ class MyTestCase(unittest.TestCase):
         el_graph.add_edge(ltk)
 
         # sam has 5 friends
-        stt = edge(sam, tom)
-        stb = edge(sam, bob)
-        stl = edge(sam, lee)
-        stz = edge(sam, zahir)
-        sta = edge(sam, arun)
+        stt = edge.UnweightedEdge(sam, tom)
+        stb = edge.UnweightedEdge(sam, bob)
+        stl = edge.UnweightedEdge(sam, lee)
+        stz = edge.UnweightedEdge(sam, zahir)
+        sta = edge.UnweightedEdge(sam, arun)
         el_graph.add_edge(stt)
         el_graph.add_edge(stb)
         el_graph.add_edge(stl)
@@ -87,36 +87,50 @@ class MyTestCase(unittest.TestCase):
         el_graph.add_edge(sta)
 
         # katie has 3 friends
-        ktr = edge(katie, rama)
-        ktl = edge(katie, lee)
-        kts = edge(katie, swati)
+        ktr = edge.UnweightedEdge(katie, rama)
+        ktl = edge.UnweightedEdge(katie, lee)
+        kts = edge.UnweightedEdge(katie, swati)
         el_graph.add_edge(ktr)
         el_graph.add_edge(ktl)
         el_graph.add_edge(kts)
 
         # zahir has 4 friends
-        zts = edge(zahir, sam)
-        zta = edge(zahir, arun)
-        ztz = edge(zahir, swati)
-        ztl = edge(zahir, lee)
+        zts = edge.UnweightedEdge(zahir, sam)
+        zta = edge.UnweightedEdge(zahir, arun)
+        ztz = edge.UnweightedEdge(zahir, swati)
+        ztl = edge.UnweightedEdge(zahir, lee)
         el_graph.add_edge(zts)
         el_graph.add_edge(zta)
         el_graph.add_edge(ztz)
         el_graph.add_edge(ztl)
 
         # swati has 3 friends
-        stk = edge(swati, katie)
-        sts = edge(swati, lee)
-        st2 = edge(swati, zahir)
+        stk = edge.UnweightedEdge(swati, katie)
+        sts = edge.UnweightedEdge(swati, lee)
+        st2 = edge.UnweightedEdge(swati, zahir)
         el_graph.add_edge(stk)
         el_graph.add_edge(sts)
         el_graph.add_edge(st2)
 
         # arun has 2 friends
-        atz = edge(arun, zahir)
-        ats = edge(arun, sam)
+        atz = edge.UnweightedEdge(arun, zahir)
+        ats = edge.UnweightedEdge(arun, sam)
         el_graph.add_edge(atz)
         el_graph.add_edge(ats)
+
+        print(el_graph.are_vertices_connected(rama, ella))
+        print(el_graph.are_vertices_connected(rama, tom))
+
+        adjancent_vertices = el_graph.get_adjancent_vertices(rama)
+        for items in adjancent_vertices:
+            print(items.value + " is friends with " + rama.value)
+
+        adjancent_vertices = el_graph.get_adjancent_vertices(sam)
+        for items in adjancent_vertices:
+            print(items.value + " is friends with " + sam.value)
+
+        el_graph.print_all_edges()
+        el_graph.print_all_vertices()
 
 
 if __name__ == '__main__':
